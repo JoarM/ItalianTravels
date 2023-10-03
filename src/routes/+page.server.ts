a146216807f91a340b0a4d770bfa54e7885dce0a
JoarM <joar.maltesson@gmail.com>
2023-10-03 22:03:57 +0200

    Added airports to home page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,10 @@
 import { db } from '$lib/db';
-import { flights } from '$lib/db/schema';
+import { airports, flights } from '$lib/db/schema';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	return {
-		flights: await db.select().from(flights)
+		flights: await db.select().from(flights).limit(10),
+		airports: await db.select().from(airports).limit(10)
 	};
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
-    import type { PageData } from "./$types";
+	import { buttonVariants } from "$lib/components/ui/button";
+    import { airports, flights } from "$lib/db/schema";
+	import { cn } from "$lib/utils";
+import type { PageData } from "./$types";
 
     export let data: PageData;
 </script>
 
-<h1>Flights</h1>
+<main class="mx-auto w-full max-w-6xl grid place-items-center py-12">
+    <h1 class="text-4xl font-bold tracking-tight lg:text-5xl">Italian Flights</h1>
+    <div class="grid md:grid-cols-2 gap-8 w-full mt-4">
+        <article class="p-3 rounded-md border">
+            <h2 class="border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Fligths</h2>
+            <ul class="mt-2">
+                {#each data.flights as flight}
+                    <li>
+                        <a href="/" class={buttonVariants({ variant: "link" })}>From { flight.origin } to { flight.destination }</a>
+                    </li>
+                {/each}
+            </ul>
+            <a href="/flights" class={cn(buttonVariants({ variant: "secondary" }), "mt-2")}>View all</a>
+        </article>
 
-<ul>
-    {#each data.flights as flight}
-        <li>{ flight.id }: From { flight.origin } to { flight.destination }</li>
-    {/each}
-</ul>
+        <article class="p-3 rounded-md border">
+            <h2 class="border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Airports</h2>
+            <ul class="mt-2">
+                {#each data.airports as airport}
+                    <li>
+                        <a href="/" class={buttonVariants({ variant: "link" })}>{ airport.city } ({ airport.code })</a>
+                    </li>
+                {/each}
+            </ul>
+            <a href="/airports" class={cn(buttonVariants({ variant: "secondary" }), "mt-2")}>View all</a>
+        </article>
+    </div>
+</main>
