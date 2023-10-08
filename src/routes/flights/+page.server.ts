d2ba96414e9cf8035cff579f848c62b16d91321f
Joar Maltesson <joar05@hotmail.com>
2023-10-08 15:10:07 +0200

    Added pagination and create

---
+++ src/routes/flights/+page.server.ts
@@ -0,0 +1,25 @@
+import { db } from '$lib/db';
+import { flights } from '$lib/db/schema';
+import { sql } from 'drizzle-orm';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async (request) => {
+    let start = request.url.searchParams.get("start") ? Number(request.url.searchParams.get("start")) : 0;
+    let limit = request.url.searchParams.get("start") ? Number(request.url.searchParams.get("limit")) : 10;
+    const count = await db.select({ count: sql`COUNT(*)` }).from(flights);
+    if (Number.isNaN(start)) {
+        start = 0;
+    }
+    if (Number.isNaN(limit) || limit < 5) {
+        limit = 10;
+    }
+    if (limit > 50) {
+        limit = 50;
+    }
+
+	return {
+		flights: await db.select().from(flights).offset(start).limit(limit),
+        flightCount: count[0].count as number,
+        limit: limit,
+	};
+}
--- src/routes/flights/+page.svelte
+++ src/routes/flights/+page.svelte
@@ -1,3 +1,80 @@
-<main>
-    
+<script lang="ts">
+	import { buttonVariants } from "$lib/components/ui/button";
+	import * as Table from "$lib/components/ui/table";
+	import { cn } from "$lib/utils";
+	import type { PageData } from "./$types";
+    import { createPagination, melt } from '@melt-ui/svelte';
+    import { ChevronLeft, ChevronRight } from 'lucide-svelte';
+
+    export let data: PageData;
+
+    const {
+        elements: { root, pageTrigger, prevButton, nextButton },
+        states: { pages, range },
+    } = createPagination({
+        count: data.flightCount,
+        perPage: data.limit,
+        defaultPage: 1,
+        siblingCount: 1,
+    });
+
+    function formatTime(minutes: number | null): string {
+        if (!minutes) return "";
+        return `${Math.floor(minutes / 60)} hours ${minutes % 60} minutes`
+    }
+</script>
+
+<main class="mx-auto w-full max-w-6xl py-12">
+    <h1 class="text-4xl font-bold tracking-tight lg:text-5xl">Fligths</h1>
+
+    <Table.Root class="mt-8 p-4">
+        <Table.Header>
+            <Table.Row>
+                <Table.Head class="w-28">Id</Table.Head>
+                <Table.Head>From</Table.Head>
+                <Table.Head>To</Table.Head>
+                <Table.Head class="text-right">Duration</Table.Head>
+            </Table.Row>
+        </Table.Header>
+        <Table.Body>
+            {#each data.flights as flight}
+                <Table.Cell>{ flight.id }</Table.Cell>
+                <Table.Cell>{ flight.origin }</Table.Cell>
+                <Table.Cell>{ flight.destination }</Table.Cell>
+                <Table.Cell class="text-right">{ formatTime(flight.duration) }</Table.Cell>
+            {/each}
+        </Table.Body>
+    </Table.Root>
+    {#if data.flights.length}
+        <p class="mt-2 text-sm text-muted-foreground">Showing flight { $range.start } - { $range.end } of { data.flightCount }</p>
+    {:else}
+        <p class="text-sm font-normal mt-4">No content for this selection.</p>
+    {/if}
+
+    <nav
+    class="flex flex-col items-center gap-4"
+    aria-label="pagination"
+    use:melt={$root}
+    >
+        <div class="flex items-center gap-2">
+            <button
+            class={cn(buttonVariants({ variant: "outline" }))}
+            use:melt={$prevButton}><ChevronLeft class="square-4" /></button>
+            {#each $pages as page (page.key)}
+            {#if page.type === 'ellipsis'}
+                <span>...</span>
+            {:else}
+                <button
+                class={cn(buttonVariants({ variant: "outline" }))}
+                use:melt={$pageTrigger(page)}>{page.value}</button
+                >
+            {/if}
+            {/each}
+            <button
+            class={cn(buttonVariants({ variant: "outline" }))}
+            use:melt={$nextButton}><ChevronRight class="square-4" /></button
+            >
+        </div>
+    </nav>
+
 </main>
